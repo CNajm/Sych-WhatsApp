082067aced9ba6360a46aba7263ae8088100c929
CNajm <charbel262@gmail.com>
2018-01-30 22:10:24 -0800

    reworked event loop and simplified sending messages through bot framework

--- Sych/cb01.py
+++ Sych/cb01.py
@@ -22,29 +22,23 @@ cmdsymbol = "%"
 class bot:
     def __init__(self, whatsappInstance):
         self.client = whatsappInstance
+        self.message_history = deque(maxlen=20)
         self.history = deque(maxlen=20)
-        
+
         self.q = queue.Queue()
         self.lock = threading.Lock()
         self.check_msgs = True
-        self.message_history = deque(maxlen=20)
-        
+
         # TODO: make prefix a class attribute instead of passing it with every send_message call
 
+    def send_message(self, to, msg, prefix=prefix):
+        with self.lock:
+            self.client.send_message(to, msg, prefix)
+
     def handle_command(self, message):
-        """
-        1. Check if message object exists in history, if it is then we should exit as to not infinitely loop.
-        2. Check if message has the format desired for a command and do stuff if True.
-        """
-        for i in self.history:
-            if message == i:
-                return
-
-
         # TODO: Eliminate repetitive command code/more scalable format
-        def is_command(command, prefix=cmdsymbol, msg=message):
-            if msg.content.lower().startswith(prefix+command):
-                self.history.append(message) # add this message object to history to signal that it has been acted on
+        def command(command, prefix=cmdsymbol, msg=message):
+            if msg.content.lower().startswith(prefix + command):
                 return True
             else:
                 return False
@@ -61,28 +55,26 @@ class bot:
             return context
 
 
-        def send_message(to, msg, prefix):
-            with self.lock:
-                self.client.send_message(to, msg, prefix)
 
-        if is_command("test"):
+
+        if command("test"):
             """
             Command
                 Responds with "I'm alive!"
             """
-            send_message(group, "I'm alive!", prefix)
+            self.send_message(group, "I'm alive!")
 
 
-        elif is_command("today"):
+        elif command("today"):
             """
             Command
                 Responds with today's date + time (12hr format)
             """
             n = datetime.datetime.now().strftime("%A %B %d @ %I:%M %p UTC-8")
-            send_message(group, "today is {}".format(n), prefix)
+            self.send_message(group, "today is {}".format(n))
 
 
-        elif is_command("yt"):
+        elif command("yt"):
             """
             Command
                 Queries youtube for "titanic theme song" and responds with name + url of first result
@@ -92,13 +84,12 @@ class bot:
             url = get_context("yt")
             try:
                 vid = ext.youtubeSearch.SearchVid(url)[0]
-                with self.lock:
-                    self.client.send_message(group, "```{}```\n{}".format(vid[0], vid[1]), prefix)
+                self.send_message(group, "```{}```\n{}".format(vid[0], vid[1]))
             except Exception as e:
                 print(e)
 
 
-        elif is_command("wiki"):
+        elif command("wiki"):
             """
             Command
                 Queries wikipedia using the Opensearch API and responds with wiki page title, url, and short description
@@ -116,38 +107,35 @@ class bot:
             else:
                 msgdata = "No results found for _{}_".format(query)
 
-            with self.lock:
-                self.client.send_message(group, msgdata, prefix)
-                
-                
-                
-                
+            self.send_message(group, msgdata)
+
+
     # TODO: Figure out which sleep-deprived 4 AM coding session synthesized this event loop
     def message_checker(self):
         """
         Since there's no way to detect new messages, we make our own:
-    
+
         Updates message_history with messages.
         Message Detection step by step:
             1. Grab latest messages up to limit (get_message second param. Think of limit as the look-behind range.)
             2. Append them to list of all messages
             3. Eliminate duplicate/non-unique messages by converting to a set() and back.
-    
+
         This allows us to detect new messages by monitoring message_history for new entries.
         """
         while self.check_msgs:
             with self.lock:
-                check = x.get_message(group, 3)
+                check = x.get_message(group, 3) # How many prev. messages to get
             try:
                 for m in check:
                     self.message_history.append(m)
             except TypeError: # This happens when getting the messages fails while they are still loading
                 print("typeerror")
                 pass
-    
+
             self.message_history = deque(list(set(self.message_history)), maxlen=20) # Convert list to set to remove duplicate Message objects. See Message class for hashing specifics.
-            time.sleep(2)
-    
+            time.sleep(2) # How many seconds to wait between checks
+
     def queue_handler(self):
         while True:
             item = self.q.get()
@@ -155,18 +143,29 @@ class bot:
                 break
             func = item[0]
             args = item[1:]
-            func(*args) # call func with args under thread lock
-            
+            func(*args)
+
+    def messageIsNew(self, message):
+        for old_message in self.history:
+            if message == old_message:
+                return False
+        return True
+
     def run(self):
+        """
+        1. Check if message object is new
+        2. If it is, then
+            add it to the queue to be handled
+            add it to the command history so it is not looped over again.
+        """
         mc = threading.Thread(target=self.message_checker)
         qh = threading.Thread(target=self.queue_handler)
         mc.start()
         qh.start()
         while True:
-            for m in list(self.message_history).reverse(): # Pause here
-                self.q.put(self.handle_command(m))        
-
+            for m in reversed(list(self.message_history)): # Handle oldest messages first
+                if self.messageIsNew(m):
+                    self.q.put(self.handle_command(m)) # If message is new, handle it and add it to history so we don't handle it again.
+                    self.history.append(m)
 
 b = bot(x).run()
-
-
